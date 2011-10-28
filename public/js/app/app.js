2cbe61085556b0fc75bb3c01b05606a5805ecd92
Chase Wilson <chase@agroism.com>
2011-10-28 14:00:41 -0700

    Adding events for the Facebook/GoogleMaps apis loading. :)

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -3,20 +3,39 @@ Object.append(APP, new Events,new Options, {
   initialize: function(options){
     this.setOptions(options);
     this.addFB();
+    this.addGoogleMaps();
   },
   addFB: function(){
     var self = this;
+    // this will add the Facebook sdk to the page.
+    fbAsyncInit = function(){
+      self.fbInit();
+    };
+    
     var script = new Element('script',{
       src: "//connect.facebook.net/en_US/all.js",
       defer:"defer",
     }).inject(document.head);
-    fbAsyncInit = function(){
-      self.fireEvent('FB.Ready:latched');
+  },
+  
+  fbInit: function(){
+    this.fireEvent('FB.Ready:latched');
+  },
+  
+  addGoogleMaps: function(){
+    var self = this;
+    googleMapsAsyncInit = function(){
+      self.googleMapsInit();
     };
+    // this will add the Facebook sdk to the page
+    var script = new Element('script',{
+      src: "//maps.googleapis.com/maps/api/js?sensor=false&callback=googleMapsAsyncInit",
+      defer:"defer",
+    }).inject(document.head);
     
-    this.addEvent('FB.Ready', function(){
-      console.log('Facebook is ready');
-    })
+  },
+  googleMapsInit: function(){
+    this.fireEvent('GoogleMaps.Ready:latched');
   }
 });
 
