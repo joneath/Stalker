0ac852cd2e2c7e26304bb01e2b25740e3dea6b9a
Chase Wilson <chase@agroism.com>
2011-10-28 18:17:50 -0700

    Adding mroe things to element

--- public/js/utils/Utils.js
+++ public/js/utils/Utils.js
@@ -32,18 +32,37 @@ matchesExactly = function(expected, actual) {
 
 Element.implement({
   hide: function(){
-  		var d;
-  		try {
-  			//IE fails here if the element is not in the dom
-  			d = this.getStyle('display');
-  		} catch(e){}
-  		if (d == 'none') return this;
-  		return this.store('element:_originalDisplay', d || '').setStyle('display', 'none');
-  	},
+    isDisplayed: function(){
+      return this.getStyle('display') != 'none';
+    },
 
-  	show: function(display){
-  		if (!display && this.isDisplayed()) return this;
-  		display = display || this.retrieve('element:_originalDisplay') || 'block';
-  		return this.setStyle('display', (display == 'none') ? 'block' : display);
-  	}
+    isVisible: function(){
+      var w = this.offsetWidth,
+        h = this.offsetHeight;
+      return (w == 0 && h == 0) ? false : (w > 0 && h > 0) ? true : this.style.display != 'none';
+    },
+
+    toggle: function(){
+      return this[this.isDisplayed() ? 'hide' : 'show']();
+    },
+
+    hide: function(){
+      var d;
+      try {
+        //IE fails here if the element is not in the dom
+        d = this.getStyle('display');
+      } catch(e){}
+      if (d == 'none') return this;
+      return this.store('element:_originalDisplay', d || '').setStyle('display', 'none');
+    },
+
+    show: function(display){
+      if (!display && this.isDisplayed()) return this;
+      display = display || this.retrieve('element:_originalDisplay') || 'block';
+      return this.setStyle('display', (display == 'none') ? 'block' : display);
+    },
+
+    swapClass: function(remove, add){
+      return this.removeClass(remove).addClass(add);
+    }
 });
